746e7dd4aaa2f314dd7dae8e1008f7b2897e5206
Manas Duishoev <manasduishoev@gmail.com>
2023-01-03 17:42:07 +0600

    Initial Commit

---
+++ Animal Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animal Gallery
+//
+//  Created by new on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
